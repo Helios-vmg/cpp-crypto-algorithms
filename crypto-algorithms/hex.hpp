4696fb0c80dabd36e293cd346f6dc28818d98718
Víctor M. González <helios.vmg@gmail.com>
2022-01-30 18:59:30 -0300

    Added some useful functions.

--- crypto-algorithms/hex.hpp
+++ crypto-algorithms/hex.hpp
@@ -5,6 +5,7 @@
 #include <stdexcept>
 #include <array>
 #include <cstring>
+#include <vector>
 
 namespace utility{
 
@@ -39,6 +40,31 @@ std::array<std::uint8_t, N> hex_string_to_buffer(const char *s, size_t size = 0)
 		b |= utility::hex2val(*(s++));
 	}
 	return ret;
+}
+
+inline std::string buffer_to_hex_string(const void *vsrc, size_t size){
+	std::string ret;
+	ret.reserve(size * 2);
+	auto src = (const std::uint8_t *)vsrc;
+	for (size_t i = 0; i < size; i++){
+		ret += hex_digits[src[i] >> 4];
+		ret += hex_digits[src[i] & 0xF];
+	}
+	return ret;
+}
+
+template <size_t N>
+std::string buffer_to_hex_string(const std::array<std::uint8_t, N> &src){
+	return buffer_to_hex_string(src.data(), N);
+}
+
+template <size_t N>
+std::string buffer_to_hex_string(const std::uint8_t (&src)[N]){
+	return buffer_to_hex_string(src, N);
+}
+
+inline std::string buffer_to_hex_string(const std::vector<std::uint8_t> &src){
+	return buffer_to_hex_string(src.data(), src.size());
 }
 
 }
